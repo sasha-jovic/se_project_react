a831cd76768d264b6075154b95cff9f883607fcd
Sasha Jovic <sashajovic@Sashas-MBP.attlocal.net>
2025-02-02 19:36:30 -0500

    submiting the project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
 import { getWeather, filterWeatherData } from "../../utils/weatherApi";
 import Footer from "../Footer/footer";
+import "../itemCard/itemCard";
 
 function App() {
   const [weatherData, setWeatherData] = useState({
